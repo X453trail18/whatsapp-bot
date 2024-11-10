2dae4590ddee445463dfca6b80c07ebda4a4da3f
X453trail18 <x453trail18@gmail.com>
2024-11-10 15:24:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ DEBUG =
 module.exports = {
   VERSION: "v1.2.8",
   SESSION:
-    process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
+    process.env.ASENA_SESSION === undefined ? "levanter_105f12a60cffb44cc8bba3d5f7f7094df6" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
     process.env.LANGUAGE === undefined
